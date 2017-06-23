aecefd199e3b7a198c0a3fdf44f005ac118b8f16
Stuart Rowlands <stuart.rowlands@acquia.com>
2017-06-23 11:48:39 +1000

    HKCU-126: Add all default configuration + custom themes.

---
+++ docroot/themes/custom/cityu/js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Custom scripts for theme.
+ */
+
+(function ($, Drupal, document) {
+  'use strict';
+
+  Drupal.behaviors.example = {
+    attach: function (context, settings) {
+      // Blank behavior.
+    }
+  }
+})(jQuery, Drupal, document);
